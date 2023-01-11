d998d8403b71b3f60e38ae54296df7c48d0c1ff1
Paulo Pinto <gomescasador@gmail.com>
2023-01-11 15:41:35 +0000

    Exercício de revisão corrigido (verificador de idade)
    O usuário podia colocar qualquer valor ou mesmo nada que gerava um resultado, ex: 2023 anos, quando deixava em branco o campo de ano de nascimento.

--- javascript/Revisao_testes/script.js
+++ javascript/Revisao_testes/script.js
@@ -14,20 +14,25 @@ function verificar() {
         sexo = 'feminino'
     }
 
-    if (idade < 10) {
-        fase = 'bebe'
-    }
-    else if (idade < 18) {
-        fase = 'jovem'
-    }
-    else if ( idade < 50) {
-        fase = 'adulto'
+    if (anoNasci < 1900 || anoNasci > 2023) {
+        res.innerHTML = `Não foi possível calcular com o ano de nascimento informado, tente novamente!`
     }
     else {
-        fase = 'idoso'
+        if (idade < 10) {
+            fase = 'bebe'
+        }
+        else if (idade < 18) {
+            fase = 'jovem'
+        }
+        else if ( idade < 50) {
+            fase = 'adulto'
+        }
+        else {
+            fase = 'idoso'
+        }
+            
+        res.innerHTML = `Detectamos uma pessoa do sexo ${sexo} que completa ${idade} anos em ${anoAgora}.`
+    
+        document.querySelector('#iimagem').src = `imagens/foto-${fase}-${sexo}.png`
     }
-        
-    res.innerHTML = `Detectamos uma pessoa do sexo ${sexo} que completa ${idade} anos em ${anoAgora}.`
-
-    document.querySelector('#iimagem').src = `imagens/foto-${fase}-${sexo}.png`
 }
